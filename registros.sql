41c1e1bc22523656d1cc4849cf19c5713c4eae05
luis eduardo jimenez <luiejimenez11111@misena.edu.co>
2021-06-03 18:45:23 -0500

    chore: proyecto deudas

--- registros.sql
+++ registros.sql
@@ -4,3 +4,9 @@ insert into proyecto.system_user1 values (3, 'OscarRamirez', 'tghy1423', 'oscmra
 insert into proyecto.system_user1 values (4, 'KatherineTorres', 'thorzafis', 'katherine.torresmor@gmail.com', null,'4', null, null, null, null);
 insert into proyecto.system_user1 values (5, 'SmithDiaz', 'ascv4589', 'diazlorena817@gmail.com', null,'5', null, null, null, null);
 
+insert into proyecto.deudas values (1, 'banco bbva', 500000, 36, 19, '01-09-2001', '01-09-2004');
+insert into proyecto.deudas values (2, 'banco av villas', 600000, 36, 19, '01-09-2001', '01-09-2004');
+insert into proyecto.deudas values (3, 'bancolombia', 700000, 36, 19, '01-09-2001', '01-09-2004');
+insert into proyecto.deudas values (4, 'banco davivienda', 400000, 36, 19, '01-09-2001', '01-09-2004');
+insert into proyecto.deudas values (5, 'banco de bogota', 300000, 36, 19, '01-09-2001', '01-09-2004');
+
