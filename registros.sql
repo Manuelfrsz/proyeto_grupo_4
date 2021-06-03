90655d4d414cc6ab889e492cb172e5e0eaeb428d
Manuel Fernando Romero Suarez <mfromero783@misena.edu.co>
2021-06-03 18:28:07 -0500

    chore: registros ingresos

--- registros.sql
+++ registros.sql
@@ -9,3 +9,9 @@ insert into proyecto.ingresos_ocasionales values (2, 'domicilios', 5, 40000);
 insert into proyecto.ingresos_ocasionales values (3, 'ninguno', 0, 0);
 insert into proyecto.ingresos_ocasionales values (4, 'Encargos del la casa', 1, 50000);
 insert into proyecto.ingresos_ocasionales values (5, 'manicures', 7, 10000);
+
+insert into proyecto.ingresos values (1, 1, 1, 454000, 654000);
+insert into proyecto.ingresos values (2, 2, 2, 454000, 654000);
+insert into proyecto.ingresos values (3, 3, 3, 454000, 454000);
+insert into proyecto.ingresos values (4, 4, 4, 454000, 504000);
+insert into proyecto.ingresos values (5, 5, 5, 454000, 524000);
