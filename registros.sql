087e71b48980e7b2e5daf527ba950fc4677b7968
Manuel Fernando Romero Suarez <mfromero783@misena.edu.co>
2021-06-03 18:17:37 -0500

    chore: registros system_user1

---
+++ registros.sql
@@ -0,0 +1,5 @@
+insert into proyecto.system_user1 values (1, 'manuelfrsz', 'qwer172839', 'mfromero783@misena.edu.co', null,'1', null, null, null, null);
+insert into proyecto.system_user1 values (2, 'luisJimenez', 'poiu192837', 'Luiejimenez11111@misena.edu.co', null,'2', null, null, null, null);
+insert into proyecto.system_user1 values (3, 'OscarRamirez', 'tghy1423', 'oscmramirez111@misena.edu.co', null,'3', null, null, null, null);
+insert into proyecto.system_user1 values (4, 'KatherineTorres', 'thorzafis', 'katherine.torresmor@gmail.com', null,'4', null, null, null, null);
+insert into proyecto.system_user1 values (5, 'SmithDiaz', 'ascv4589', 'diazlorena817@gmail.com', null,'5', null, null, null, null);
