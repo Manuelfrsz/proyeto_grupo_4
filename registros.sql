75f31e239c629acb61cb8ad20fddab29378af9d6
Manuel Fernando Romero Suarez <mfromero783@misena.edu.co>
2021-06-03 18:19:24 -0500

    chore: registros  ingresos_ocasionales

--- registros.sql
+++ registros.sql
@@ -3,3 +3,9 @@ insert into proyecto.system_user1 values (2, 'luisJimenez', 'poiu192837', 'Luiej
 insert into proyecto.system_user1 values (3, 'OscarRamirez', 'tghy1423', 'oscmramirez111@misena.edu.co', null,'3', null, null, null, null);
 insert into proyecto.system_user1 values (4, 'KatherineTorres', 'thorzafis', 'katherine.torresmor@gmail.com', null,'4', null, null, null, null);
 insert into proyecto.system_user1 values (5, 'SmithDiaz', 'ascv4589', 'diazlorena817@gmail.com', null,'5', null, null, null, null);
+
+insert into proyecto.ingresos_ocasionales values (1, 'Paseo_perros', 4, 50000);
+insert into proyecto.ingresos_ocasionales values (2, 'domicilios', 5, 40000);
+insert into proyecto.ingresos_ocasionales values (3, 'ninguno', 0, 0);
+insert into proyecto.ingresos_ocasionales values (4, 'Encargos del la casa', 1, 50000);
+insert into proyecto.ingresos_ocasionales values (5, 'manicures', 7, 10000);
